a83ed0513c0c15f1bf4315428a67fbb97e769985
Sinan UYĞUN <sinan_uygun@windowslive.com>
2023-12-13 19:27:49 +0300

    add json file read

--- src/main/java/com/lucida/lucida/Json_processes.java
+++ src/main/java/com/lucida/lucida/Json_processes.java
@@ -1,10 +1,21 @@
 package com.lucida.lucida;
+
 import java.io.FileWriter;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 class Json_processes {
-    public static void json_write(String name,int weight,int height,String key) {
+    public static void json_write(String name,String weight,String height,String key) {
         JSONObject data = new JSONObject();
         data.put("name", name);
         data.put("weight", weight);
@@ -22,4 +33,21 @@ class Json_processes {
             e.printStackTrace();
         }
     }
+    public static String json_read(String json_patch,String desired) throws IOException, ParseException {
+        JSONParser parser=new JSONParser();
+        JSONObject data= (JSONObject) parser.parse(new FileReader(json_patch));
+        String select= (String) data.get(desired);
+        return select;
+        }
+    public static String[] json_all_read(String json_patch,String desired) throws IOException, ParseException {
+        JSONParser parser=new JSONParser();
+        JSONObject data= (JSONObject) parser.parse(new FileReader(json_patch));
+        String[] select=new String[4];
+        select[0]= (String) data.get("name");
+        select[1]= (String) data.get("weight");
+        select[2]= (String) data.get("height");
+        select[3]= (String) data.get("key");
+        return select;
+    }
+
 }
