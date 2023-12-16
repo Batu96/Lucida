5d4470fe7772914c30d74703cb1476c85e16fe75
Batu96 <71096452+Batu96@users.noreply.github.com>
2023-12-16 22:33:57 +0300

    JavaFX version switch

--- src/main/java/com/lucida/lucida/InfoStage.java
+++ src/main/java/com/lucida/lucida/InfoStage.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -15,6 +16,8 @@ public class InfoStage extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+
         Pane root = new Pane();
         WebView webView = new WebView();
 
@@ -22,21 +25,35 @@ public class InfoStage extends Application {
         mov1.setLayoutX(10);
         mov1.setLayoutY(10);
         mov1.setOnAction(e -> {
-            webView.getEngine().load("https://www.oldschoollabs.com/wp-content/uploads/2021/01/00271301-Barbell-Bent-Over-Row_Back-OPTIMIZED-1.gif");
-            webView.setPrefSize(300, 400);
-            webView.setLayoutX(300);
+            webView.getEngine().load("https://giphy.com/embed/4ssD3no5DCbQs");
+            webView.setPrefSize(400, 390);
+            webView.setLayoutX(200);
             webView.setLayoutY(0);
             root.getChildren().add(webView);
         });
-        Button mov2 = new Button("");
+        Button mov2 = new Button("DeadLift");
         mov2.setLayoutX(10);
         mov2.setLayoutY(50);
-
-        Button mov3 = new Button("");
+        mov2.setOnAction(e -> {
+            webView.getEngine().load("https://giphy.com/embed/4ssD3no5DCbQs");
+            webView.setPrefSize(400, 390);
+            webView.setLayoutX(200);
+            webView.setLayoutY(0);
+            root.getChildren().add(webView);
+        });
+        Button mov3 = new Button("PullUp");
         mov3.setLayoutX(10);
         mov3.setLayoutY(90);
+        mov3.setOnAction(e -> {
+            webView.getEngine().load("https://giphy.com/embed/4ssD3no5DCbQs");
+            webView.setPrefSize(400, 390);
+            webView.setLayoutX(200);
+            webView.setLayoutY(0);
+            root.getChildren().add(webView);
+        });
 
         root.getChildren().addAll(mov1, mov2, mov3);
+        stage.setTitle("Hareketler");
         stage.setScene(new Scene(root, 700, 400));
         stage.show();
     }
